2e917913ce353a50d1cb1232059f87565b68ad79
lilpolymath <ayofavour7@gmail.com>
2020-01-07 10:58:49 +0100

    Updates

---
+++ precache-manifest.c6fc301d6f74cbb2fdce8cd2e1b151a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfb707f7a591bb6bd5ff0daa9fa731a",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "2933b453fd17507a9771",
+    "url": "/learn-react/static/css/main.2d7fc82a.chunk.css"
+  },
+  {
+    "revision": "1016aa7c48950ce2dab2",
+    "url": "/learn-react/static/js/2.6918f563.chunk.js"
+  },
+  {
+    "revision": "2933b453fd17507a9771",
+    "url": "/learn-react/static/js/main.802f83f9.chunk.js"
+  },
+  {
+    "revision": "1f5a4aa29338276e39d8",
+    "url": "/learn-react/static/js/runtime~main.1bcabeff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackernews/precache-manifest.63e891f9173c842352d3e0ca6ddc65e3.js"
+  "/learn-react/precache-manifest.c6fc301d6f74cbb2fdce8cd2e1b151a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackernews/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learn-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
