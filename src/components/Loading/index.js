3ee4c251d46305d09705e62de1fdfc1e0e228766
lilpolymath <ayofavour7@gmail.com>
2020-01-08 17:11:52 +0100

    feat: changes to Loading style.

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,22 +1,5 @@
 import React from 'react';
 
-const Loading = () => (
-  <div class='loadingio-spinner-spinner-1vozakddyad'>
-    <div class='ldio-515e7my2t64'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </div>
-);
+const Loading = () => <div>Loading...</div>;
 
 export default Loading;
